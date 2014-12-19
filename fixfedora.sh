ea44d6ace388e1511c4d1e5c29c6d9e2d348ab55
Kevin Bowrin <kevin.bowrin@carleton.ca>
2014-12-19 13:06:45 -0500

    Add default transform to indexing rdf update.

--- fixfedora.sh
+++ fixfedora.sh
@@ -7,3 +7,4 @@ curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagr
 curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagrant/indexing.rdf" "http://localhost:8080/fcrepo/rest/root/test"
 curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagrant/title.rdf" "http://localhost:8080/fcrepo/rest/root/test"
 
+curl -X POST -H "Content-Type: application/json" --data-binary "@/vagrant/uuid-solr-field.json" "http://localhost:8983/solr/schema/fields"
