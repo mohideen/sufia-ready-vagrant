2c7ce44a3637c741b8d5ddd4b33b69cbf6a6a462
Kevin Bowrin <kevin.bowrin@carleton.ca>
2014-12-18 19:39:09 -0500

    Removed unnecessary file.

--- tomcat_fedora_solr.sh
+++ tomcat_fedora_solr.sh
@@ -1,21 +1,42 @@
 #!/usr/bin/env bash
 
 echo "Installing Java and Tomcat..."
-yum -y install java tomcat tomcat-admin-webapps
+yum -y install java tomcat
+
+echo "Installing Fedora 4..."
+mkdir -p /var/fedora4/fedora-data
+chown -R tomcat:tomcat /var/fedora4
+wget -q http://repo1.maven.org/maven2/org/fcrepo/fcrepo-webapp/4.0.0/fcrepo-webapp-4.0.0.war -O /usr/share/tomcat/webapps/fcrepo.war
 
 echo "Installing Solr..."
-
 wget -q http://mirror.csclub.uwaterloo.ca/apache/lucene/solr/4.10.2/solr-4.10.2.tgz -O /tmp/solr.tar.gz
 tar xvf /tmp/solr.tar.gz -C /tmp
 cp /tmp/solr-4.10.2/example/lib/ext/* /usr/share/tomcat/lib/
 mkdir -p /var/solr
 cp -R /tmp/solr-4.10.2/example/solr/* /var/solr
+cp /vagrant/solrconfig.xml /var/solr/collection1/conf/solrconfig.xml
 chown -R tomcat:tomcat /var/solr
 cp /tmp/solr-4.10.2/dist/solr-4.10.2.war /usr/share/tomcat/webapps/solr.war
 
-grep -q -F 'JAVA_OPTS="-Dsolr.solr.home=/var/solr"' /etc/tomcat/tomcat.conf || echo 'JAVA_OPTS="-Dsolr.solr.home=/var/solr"' >> /etc/tomcat/tomcat.conf
+echo "Installing Message Consumer..."
+wget -q http://repo1.maven.org/maven2/org/fcrepo/fcrepo-message-consumer-webapp/4.0.0/fcrepo-message-consumer-webapp-4.0.0.war -O /usr/share/tomcat/webapps/fcrepo-message-consumer.war
+
+echo "Setting JAVA_OPTS..."
+grep -q -F 'JAVA_OPTS="-Dfcrepo.home=/var/fedora4/fedora-data -Dsolr.solr.home=/var/solr -DsolrIndexer.port=8080"' /etc/tomcat/tomcat.conf || echo 'JAVA_OPTS="-Dfcrepo.home=/var/fedora4/fedora-data -Dsolr.solr.home=/var/solr -DsolrIndexer.port=8080"' >> /etc/tomcat/tomcat.conf
 
 systemctl enable tomcat.service
 systemctl start tomcat.service
+
+echo "Installing Fuseki..."
+wget -q http://www.apache.org/dist/jena/binaries/jena-fuseki-1.1.1-distribution.tar.gz -O /home/vagrant/jena-fuseki-1.1.1-distribution.tar.gz
+tar xvfz /home/vagrant/jena-fuseki-1.1.1-distribution.tar.gz
+cd /home/vagrant/jena-fuseki-1.1.1/
+mkdir -p /var/fuseki
+chown -R tomcat:tomcat /var/fuseki
+nohup ./fuseki-server --loc /var/fuseki  /fcrepo &
+
+sleep 20
+
+/vagrant/fixfedora.sh
 
 echo "Done!"
