5cb91c4b1a2c8909fbcc9a1587a22fe7c6d823f6
Kevin Bowrin <kevin.bowrin@carleton.ca>
2014-12-19 13:56:25 -0500

    Changed order of curl commands.

--- fixfedora.sh
+++ fixfedora.sh
@@ -2,9 +2,10 @@
 
 curl -X PUT "http://localhost:8080/fcrepo/rest/root"
 curl -X PUT "http://localhost:8080/fcrepo/rest/root/test"
+
+curl -X POST -H "Content-Type: application/json" --data-binary "@/vagrant/uuid-solr-field.json" "http://localhost:8080/solr/schema/fields"
+
 curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagrant/indexing.rdf" "http://localhost:8080/fcrepo/rest/root"
 curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagrant/roottitle.rdf" "http://localhost:8080/fcrepo/rest/root"
 curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagrant/indexing.rdf" "http://localhost:8080/fcrepo/rest/root/test"
 curl -X PATCH -H "Content-Type: application/sparql-update" --data-binary "@/vagrant/title.rdf" "http://localhost:8080/fcrepo/rest/root/test"
-
-curl -X POST -H "Content-Type: application/json" --data-binary "@/vagrant/uuid-solr-field.json" "http://localhost:8080/solr/schema/fields"
